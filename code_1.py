eec9f79854381069fb2b036b2c495aad9f3ff26e
datom0508 <anhtaitran923@gmail.com>
2025-05-19 22:33:40 +0700

    complete the game

--- code_1.py
+++ code_1.py
@@ -7,8 +7,14 @@ pygame.init()
 
 background  = pygame.image.load("./asset/img/background.png")
 ground = pygame.image.load("./asset/img/ground.png")
+restart_btn_img = pygame.image.load("./asset/img/restart.png")
 
 clock = pygame.time.Clock()
+
+#font chữ
+font = pygame.font.SysFont('Bauhaus 93', 60)
+# màu chữ
+white = (255, 255, 255)
 
 # game variables
 fps = 60
@@ -25,7 +31,23 @@ game_over = False
 pipe_gap = 150 #khoảng cách giữa 2 ống
 pipe_frequency = 1500 #tần suất xuất hiện 1 ống (ms)
 last_pipe = pygame.time.get_ticks() - pipe_frequency #thời gian ống cuối cùng được tạo
+pass_pipe = False #flappy đã bay qua ống hay chưa
+score = 0
 
+# hàm vẽ chữ
+def draw_text(text, font, text_colour, x, y):
+    img = font.render(text, True, text_colour)
+    screen.blit(img, (x, y))
+
+
+# reset các biến khi reset game
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height / 2)
+    flappy.velocity = 0
+    score = 0
+    return score
 
 # tao một class Bird bằng pygame.sprite
 # các sprites là các thực thể có thể chuyển động trong game, có màu sắc, hành động,...
@@ -69,7 +91,7 @@ class Bird(pygame.sprite.Sprite):
                 self.rect.y += int(self.velocity)
 
         # nhảy lên khi nhấn chuột trái
-        if game_over == False :
+        if game_over == False:
             if pygame.mouse.get_pressed()[0] and self.clicked == False:
                 self.velocity = -10
                 self.clicked = True
@@ -123,6 +145,30 @@ class Pipe(pygame.sprite.Sprite):
             self.kill()
 
 
+class Button:
+    def __init__(self, image, x, y):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+    def draw(self):
+
+        btn_clicked = False
+
+        # lấy vị trí con chuột
+        mouse_pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mouse_pos):   #kiểm tra nếu con chuột đang hover trên cái nút
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            if pygame.mouse.get_pressed()[0]:   #kiểm tra nếu chuột trái được nhấn
+                btn_clicked = True
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return btn_clicked
+
+
 # tạo mảng các sprite, mỗi phần tử là 1 dạng của flappy
 bird_group = pygame.sprite.Group()
 
@@ -134,6 +180,9 @@ flappy = Bird(100, int(screen_height/2))
 
 # thêm vào bird_group
 bird_group.add(flappy)
+
+# tạo nút restart
+restart_btn = Button(restart_btn_img, screen_width // 2 - 50, screen_height // 2 - 100)
 
 run = True
 
@@ -156,6 +205,19 @@ while run:
 
     # vẽ mặt đất
     screen.blit(ground, (ground_scroll, 535))
+
+    # tính điểm
+    if len(pipe_group) > 0:    #nếu có ít nhất 1 ống đã xuất hiện
+        if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left\
+            and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right\
+            and pass_pipe == False:
+            pass_pipe = True
+        if pass_pipe == True:
+            if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+                score += 1
+                pass_pipe = False
+
+    draw_text(str(score), font, white, int(screen_width / 2), 20)
 
     # nếu flappy đụng ống
     if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0: #2 cái false là khi 2 group chạm nhau thì sẽ không có cái nào bị xóa, true là sẽ xóa đi
@@ -192,6 +254,13 @@ while run:
         background_scroll = 0
 
     
+    # nếu game over
+    if game_over == True:
+        if restart_btn.draw():
+            game_over = False
+            score = reset_game()
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
 
     for event in pygame.event.get():
         if event.type == QUIT:
